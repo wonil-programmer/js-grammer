5a373d5496e5379c79f6f6335dddf59a7108d1a4
Wonil Heo <wonza8585@gmail.com>
2023-06-19 17:37:53 +0900

    23.6.19(2)

---
+++ 5.promise-egg.js
@@ -0,0 +1,23 @@
+function fetchEgg(chicken) {
+    return Promise.resolve(`${chicken} => 🥚`);
+}
+function fryEgg(egg) {
+    return Promise.resolve(`${egg} => 🍳`);
+}
+function getChicken() {
+    return Promise.reject(new Error('치킨을 가져올 수 없음!'));
+    // return Promise.resolve(`🌳 => 🐓`);
+}
+
+getChicken()
+.catch(error =>  {
+    console.log(error.name);
+    return '🐔';
+})
+// .then(chicken => fetchEgg(chicken))
+// .then(egg => fryEgg(egg))
+// .then(friedEgg => console.log(friedEgg))
+// 아래와 같이 축약하여 가독성 높임
+.then(fetchEgg(chicken))
+.then(fryEgg(egg))
+.then(console.log);
